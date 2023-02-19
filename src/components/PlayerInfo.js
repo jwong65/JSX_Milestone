c28ef80ff369c704e057c3dc8a75a4531815ca59
jwong65 <jason.wong6507@gmail.com>
2023-02-19 09:37:43 -0500

    Added the health component and styled it so it would fit into the info box

--- src/components/PlayerInfo.js
+++ src/components/PlayerInfo.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import Health from './Health'
+
+import '../CSS/styles.css'
 
 export default function PlayerInfo() {
   return (
-    <div className='playerinfo'>PlayerInfo</div>
+    <div className='playerinfo'>PlayerInfo
+        <Health />
+    </div>
   )
 }
