68ec6003c62b53951625e4c06c9fda3df6dd41fe
jwong65 <jason.wong6507@gmail.com>
2023-02-28 17:47:00 -0500

    Added Navbar component using bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import NavigationBar from './components/NavigationBar';
 import Hand from './components/Hand'
 import PlayerInfo from './components/PlayerInfo';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
   <>
+    <NavigationBar />
     <PlayerInfo />
     <Hand />
   </>
---
+++ src/components/NavigationBar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Nav, Navbar, Container } from 'react-bootstrap'
+
+export default function NavigationBar() {
+  return (
+    <Navbar>
+        <Container>
+            <Navbar.Toggle aria-controls='basic-navbar-nav' />
+            <Nav className='justify-content flex-grow-1'>
+                <Nav.Link href='https://github.com/jwong65/JSX_Milestone'>Repository</Nav.Link>
+            </Nav>
+        </Container>
+    </Navbar>
+  )
+}
