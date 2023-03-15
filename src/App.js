66100199bf860166581e9477d2aa18e6a1dddf6b
jwong65 <jason.wong6507@gmail.com>
2023-03-15 13:22:29 -0400

    Created pages directory, which is where the react router will pull from

--- src/App.js
+++ src/App.js
@@ -3,12 +3,25 @@ import NavigationBar from './components/NavigationBar';
 import Hand from './components/Hand'
 import PlayerInfo from './components/PlayerInfo';
 
+//Importing the Pages
+import Home from './components/pages/Home';
+import Gamegit  from './components/pages/Game';
+
 import 'bootstrap/dist/css/bootstrap.min.css'
+
+// Importing react router dom
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
   <div title='Card Game'>
     <NavigationBar />
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/game' element={<Game />} />
+      </Routes>
+    </Router>
     <PlayerInfo />
     <Hand />
   </div>
