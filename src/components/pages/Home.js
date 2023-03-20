a75bf46eedf4016ceb4e0749af66b352f7fa5504
jwong65 <jason.wong6507@gmail.com>
2023-03-20 16:15:58 -0400

    Added the className to center the text of the card with bootstrap

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -9,19 +9,21 @@ export default function Home() {
   return (
     <>
       <NavigationBar />
-      <Card style={{width:'15em'}} className='card'>
-        <Card.Img variant='top' />
-        <Card.Title>Repository</Card.Title>
-        <Card.Text>Placeholder text</Card.Text>
-        <Button href='https://github.com/jwong65/JSX_Milestone' variant='primary'>Repository</Button>
-      </Card>
-      <Card style={{width: '15em'}} className='card'>
-        <Card.Img />
-        <Card.Title>Previous Project</Card.Title>
-        <Card.Text>More Placeholder text</Card.Text>
-        <Button href='' variant='info'>New Buttons</Button>
-        <Button href='' variant='danger'>Second Button</Button>
-      </Card>
+      <div className='card text-center'>
+        <Card style={{width:'15em'}} className='card-block'>
+          <Card.Img variant='top' />
+          <Card.Title>Repository</Card.Title>
+          <Card.Text>Placeholder text</Card.Text>
+          <Button href='https://github.com/jwong65/JSX_Milestone' variant='primary'>Repository</Button>
+        </Card>
+        <Card style={{width: '15em'}} className='card'>
+          <Card.Img />
+          <Card.Title>Previous Project</Card.Title>
+          <Card.Text>More Placeholder text</Card.Text>
+          <Button href='' variant='info'>New Buttons</Button>
+          <Button href='' variant='danger'>Second Button</Button>
+        </Card>
+      </div>
       <div>Home</div>
     </>
   )
