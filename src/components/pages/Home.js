6a60df99d1b414e5c7c8e8298c69ef967a0f0c1c
jwong65 <jason.wong6507@gmail.com>
2023-03-20 16:22:45 -0400

    styled buttons inside the div so it will fit in each card

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -9,19 +9,21 @@ export default function Home() {
   return (
     <>
       <NavigationBar />
-      <div className='card text-center'>
-        <Card style={{width:'15em'}} className='card-block'>
+      <div className='card text-center' style={{width:'1000px'}}>
+        <Card style={{width:'30em'}} className='card-block'>
           <Card.Img variant='top' />
           <Card.Title>Repository</Card.Title>
           <Card.Text>Placeholder text</Card.Text>
           <Button href='https://github.com/jwong65/JSX_Milestone' variant='primary'>Repository</Button>
         </Card>
-        <Card style={{width: '15em'}} className='card'>
+        <Card style={{width: '30em'}} className='card'>
           <Card.Img />
           <Card.Title>Previous Project</Card.Title>
           <Card.Text>More Placeholder text</Card.Text>
-          <Button href='' variant='info'>New Buttons</Button>
-          <Button href='' variant='danger'>Second Button</Button>
+          <div>
+            <Button style ={{width:'50%'}} href='' variant='info'>New Buttons</Button>
+            <Button style={{width:'50%'}}href='' variant='danger'>Second Button</Button>
+          </div> 
         </Card>
       </div>
       <div>Home</div>
