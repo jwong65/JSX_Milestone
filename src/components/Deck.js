c2052d7644d03d6a48ced58ed36ef45b88c40c2c
jwong65 <jason.wong6507@gmail.com>
2023-03-21 10:14:06 -0400

    Added the array of deck to a useState, put deck.length to be revealled on the deck itself

--- src/components/Deck.js
+++ src/components/Deck.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import '../CSS/card.css'
 
+import { useState } from 'react'
 
 export default function Deck() {
+    const [deck, setDeckSize] = useState([])
   return (
     <div>
         <img src={require('../assets/cardBorder.jpg')} className='deck' />
-        <div className='centered'>Centered</div>
+        <div className='centered'>{deck.length}</div>
     </div>
   )
 }
