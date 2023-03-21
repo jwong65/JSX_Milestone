7516ca482e577d3620df9acab8f88a9da428f1fc
jwong65 <jason.wong6507@gmail.com>
2023-03-21 14:28:32 -0400

    Testing PopOver for the deck, considering add trigger for draw

--- src/components/Deck.js
+++ src/components/Deck.js
@@ -2,12 +2,21 @@ import React from 'react'
 import '../CSS/card.css'
 
 import { useState } from 'react'
+import { Popover, OverlayTrigger } from 'react-bootstrap'
 
 export default function Deck() {
     const [deck, setDeck] = useState([])
+    const popoverLeft = (
+        <Popover id="popover-positioned-left" title="Popover left">
+          <strong>Holy guacamole!</strong> Check this info.
+        </Popover>
+      );
   return (
     <div>
-        <img src={require('../assets/WoodBack.png')} className='deck' />
+    
+        <OverlayTrigger trigger={['hover','hover']} placement='left' overlay={popoverLeft}>
+            <img src={require('../assets/WoodBack.png')} className='deck' />
+        </OverlayTrigger>
         <div className='centered'>{deck.length}</div>
     </div>
   )
