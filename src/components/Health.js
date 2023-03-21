6e7b6fc22d8ba3fbb20d78d148c0dfdf6980a678
jwong65 <jason.wong6507@gmail.com>
2023-03-21 09:46:01 -0400

    Added useState to track for health, considering styling the health bar

--- src/components/Health.js
+++ src/components/Health.js
@@ -1,7 +1,9 @@
 import React from 'react'
+import { useState } from 'react'
 
 export default function Health() {
+  const [health, setHealth] = useState(20)
   return (
-    <div className='health'>Health</div>
+    <div className='health'>Health: {health}</div>
   )
 }
