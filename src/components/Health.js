0d43ab5af50e91fd3c9cccea654411c4c6aabfa1
jwong65 <jason.wong6507@gmail.com>
2023-03-21 14:46:56 -0400

    Added Spinner next to health

--- src/components/Health.js
+++ src/components/Health.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import { useState } from 'react'
 
+import { Spinner } from 'react-bootstrap'
+
 export default function Health() {
   const [health, setHealth] = useState(20)
   return (
-    <div className='health'>Health: {health}</div>
+    
+    <div className='health'>
+      <Spinner animation='grow' size='sm' />
+      Health: {health}
+    </div>
   )
 }
+// Loading spinner for health animation=grow, can also use border for a loading button perhaps
