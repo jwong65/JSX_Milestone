4e4bc28bf7bb7131b8477d969b6f756a431a523c
jwong65 <jason.wong6507@gmail.com>
2023-04-02 11:37:45 -0400

    Added a button and a useState to keep track of the turn

--- src/components/info_components/GameInfo.js
+++ src/components/info_components/GameInfo.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import '../../CSS/styles.css'
 
 export default function GameInfo() {
+  const [turn, setTurn] = useState(0)
   return (
     <div className='infoplay'>
-        GameInfo
+      <div>
+        <b>This is turn {turn}.</b>
+      </div>
+      <button onClick={()=>setTurn(turn+1)}>EndTurn</button>
     </div>
   )
 }
