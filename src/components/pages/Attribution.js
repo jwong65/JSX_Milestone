603c84f39081fc4f6d63891e3be9725e12bce4c3
jwong65 <jason.wong6507@gmail.com>
2023-04-03 01:30:50 -0400

    Added attribution for the LinkedIn logo

--- src/components/pages/Attribution.js
+++ src/components/pages/Attribution.js
@@ -11,6 +11,8 @@ export default function Attribution() {
             <h1>Attribution</h1>
             <h4>Images were created by Typhen Doryk</h4>
             <p>Link to the card backs are found <a href="https://www.deviantart.com/toomanypenguins/art/Free-Card-Pack-by-Typhen-609353079">here</a></p>
+            <h4>LinkedIn Icon provided by FontAwesome</h4>
+            <p>Link to icon: <a href='https://fontawesome.com/icons/linkedin?f=brands&s=solid'>Link</a></p>
         </div>
         
     </>
