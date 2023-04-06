ebc55d2e946cd9ccce8a61ec8b557e6edbad0d2e
jwong65 <jason.wong6507@gmail.com>
2023-04-06 17:18:06 -0400

    Adjusted Footer so that it would open in new tab

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,8 +8,8 @@ import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 export default function Footer() {
   return (
-    <footer className="blockquote-footer">
-    <a href='https://www.linkedin.com/in/jason-wong-954193144'><FontAwesomeIcon icon={faLinkedin} size='6x' /> </a>
+    <footer>
+    <a href='https://www.linkedin.com/in/jason-wong-954193144'target='_blank' ><FontAwesomeIcon icon={faLinkedin} size='6x' /> </a>
     Link to my LinkedIn
     </footer>
   )
