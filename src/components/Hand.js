80bffcc44b20cf577619d147a51086e898bb9095
jwong65 <jason.wong6507@gmail.com>
2023-04-18 18:44:33 -0400

    Doing more testing for text values need to find a way to unnest the array

--- src/components/Hand.js
+++ src/components/Hand.js
@@ -12,19 +12,30 @@ export default function Hand() {
 // let newImage = document.createElement('img')
 // newImage.src ='assets/cardBorder.jpg'
 
+// Cards need to be randomized but for now let's test.
+// for (let i=0; i<deckArray.length;i++){
+//   console.log(deckArray[i])
+// }
+
 const [cardsInHand, setCards] = useState([1,2,3])
 function addCards(){
   setCards([...cardsInHand, cardsImg])
   let newDeckSize = deckArray.length()-1
 }
+// this.type = type
+// this.value =value
+// this.text = text
+// this.cardID = cardID
 const popoverUp = (
   <Popover id="popover-positioned-top" title="Popover Up">
-    Placeholder Text for each card.
+    Placeholder text for cards
   </Popover>
 );
 
+
   return (
     <div className='hand' id='hands'>
+      <div></div>
         {cardsInHand.map((index)=>(
           <div key={index}>
             {/* Considering onMouseOver={} or OverlayTrigger */}
